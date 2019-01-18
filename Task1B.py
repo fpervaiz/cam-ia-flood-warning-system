f4d253bcde104b2033d5ce66011fdfeba7d9997b
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-01-18 23:04:42 +0000

    Completed Task 1B

---
+++ Task1B.py
@@ -0,0 +1,31 @@
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+location_coord = (52.2053, 0.1218)
+
+def run():
+    """Requirements for Task 1B"""
+
+    stations = build_station_list()
+    results = stations_by_distance(stations, location_coord)
+
+    # Ten closest
+    closest = list()
+    for result in results[:10]:
+        station, distance = result
+        closest.append((station.name, station.town, distance))
+
+    # Ten furthest
+    furthest = list()
+    for result in results[-10:]:
+        station, distance = result
+        furthest.append((station.name, station.town, distance))
+    
+    print("Ten closest stations from {}:".format(location_coord))
+    print(closest)
+    print("\nTen furthest stations from {}:".format(location_coord))
+    print(furthest)
+
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,35 @@ geographical data.
 
 """
 
+import math
 from .utils import sorted_by_key  # noqa
+
+def haversine(coord1, coord2):
+    """Returns the distance in km between two coordinate points on the globe."""
+
+    lon1, lat1 = coord1
+    lon2, lat2 = coord2
+
+    R = 6371000
+    phi_1 = math.radians(lat1)
+    phi_2 = math.radians(lat2)
+
+    delta_phi = math.radians(lat2-lat1)
+    delta_lambda = math.radians(lon2-lon1)
+
+    a = math.sin(delta_phi/2.0)**2 + math.cos(phi_1)*math.cos(phi_2)*math.sin(delta_lambda/2.0)**2
+    
+    c = 2*math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    distance = round(R*c/1000, 3)
+
+    return distance
+
+def stations_by_distance(stations, p):
+    """This submodule returns a list of (station, distance) tuples sorted by distance."""
+
+    station_distance_list = list()
+    for station in stations:
+        distance = haversine(station.coord, p)
+        station_distance_list.append((station, distance))
+    return sorted_by_key(station_distance_list, 1)
