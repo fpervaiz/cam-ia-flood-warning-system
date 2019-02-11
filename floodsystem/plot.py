6da998bf388a663c642b520168b18407e9b66dc2
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-02-11 00:30:24 +0000

    Added low/high lines to plot

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,6 +8,10 @@ def plot_water_levels(station, dates, levels):
     """Creates a matplotlib plot of the water level data against time for a station
     with typical high and low levels"""
     plt.plot(dates, levels)
+    if station.typical_range:
+        low, high = station.typical_range
+        plt.axhline(low, color='r', linestyle='--')
+        plt.axhline(high, color='r', linestyle='--')
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.title(station.name)
