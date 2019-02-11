0534b835bee966408543bf94c6c55f5e4bb3efd0
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-02-11 00:15:57 +0000

    Edited 2E to run function

--- Task2E.py
+++ Task2E.py
@@ -5,12 +5,17 @@ from floodsystem.datafetcher import fetch_measure_levels
 
 import datetime
 
-stations = build_station_list()
-update_water_levels(stations)
-highest_stations = stations_highest_rel_level(stations, 5)
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    highest_stations = stations_highest_rel_level(stations, 5)
 
-for station_tuple in highest_stations:
-    station = station_tuple[0]
-    dt = 10
-    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    plot_water_levels(station, dates, levels)
+    for station_tuple in highest_stations:
+        station = station_tuple[0]
+        dt = 10
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
