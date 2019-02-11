6467bffe4dff9e93068fdf1f48f96d7417e2809b
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-02-11 00:42:08 +0000

    Task 2F progress

--- Task2F.py
+++ Task2F.py
@@ -1,7 +1,22 @@
+from floodsystem.flood  import stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.plot import plot_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.analysis import polyfit
 
+import datetime
+
+
 def run():
-    return None
+    stations = build_station_list()
+    update_water_levels(stations)
+    highest_stations = stations_highest_rel_level(stations, 5)
+
+    for station_tuple in highest_stations:
+        station = station_tuple[0]
+        dt = 2
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        poly, d0 = polyfit(dates, levels, 4)
 
 if __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,6 +1,13 @@
+from matplotlib.dates import date2num
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 def polyfit(dates, levels, p):
-    poly, d0 = polyfit(dates, levels, 3)
+    x = date2num(dates)
+    y = levels
+    d0 = x[0]
+    p_coeff = np.polyfit(x - d0, y, p)
+    poly = np.poly1d(p_coeff)
+
     return (poly, d0)
