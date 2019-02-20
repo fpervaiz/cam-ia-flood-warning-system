e564b2a27690db3aea0bf5537939cd667ccdb69d
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-02-20 21:51:13 +0000

    Unit tests for analysis and flood

---
+++ test_analysis.py
@@ -0,0 +1,31 @@
+"""Unit test for the analysis submodule"""
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.analysis import level_is_rising, polyfit, get_threat_level
+from random import choice
+
+import datetime
+import numpy as np
+
+stations = build_station_list(use_cache=True)
+update_water_levels(stations)
+
+station = choice(stations)
+
+def test_polyfit():
+    dt = 10
+    degree = 4
+    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+    poly, d0 = polyfit(dates, levels, degree)
+    assert poly
+    assert d0
+
+def test_level_is_rising():
+    dates = np.array((1, 2, 3))
+    poly = np.poly1d(np.array(1, 0, 0))
+    d0 = 0
+    assert level_is_rising(poly, d0, dates)
+
+def test_get_threat_level():
+    assert get_threat_level(True, 1.6) == "Severe"
---
+++ test_flood.py
@@ -0,0 +1,23 @@
+"""Unit test for the flood module"""
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from random import choice
+
+stations = build_station_list(use_cache=True)
+update_water_levels(stations)
+
+def test_stations_level_over_threshold():
+    tol = 0.8    
+    flooded_stations = stations_level_over_threshold(stations, tol)
+    for station_tuple in flooded_stations:
+        assert station_tuple[1] > tol
+
+def test_stations_highest_rel_level():
+    chosen_station = choice(stations)
+    chosen_station.latest_level = 999
+    highest_stations = stations_highest_rel_level(stations, 1)
+    assert highest_stations[0][0] == chosen_station.name
+    assert highest_stations[0][1] == chosen_station.latest_level
+    assert len(highest_stations) == 1
